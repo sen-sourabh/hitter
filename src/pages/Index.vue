0a9478bf28f1d9e335de5cd6dc5a10a3922a378e
Sourabh Sen <sourabhsen201313@gmail.com>
2021-08-29 14:39:56 +0530

    showing response on UI after API call

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="row">
-      <div class="col-2">
+      <div class="col-12 col-md-2">
         <q-select
           dense
           outlined
@@ -9,7 +9,7 @@
           :options="request_options"
         />
       </div>
-      <div class="col-8">
+      <div class="col-12 col-md-8">
         <q-input
           dense
           outlined
@@ -17,16 +17,65 @@
           lazy-rules
           :rules="[(val) => (val && val.length > 0) || '']"
           placeholder="https://www.example.com"
+          class="expectedRequestURL"
         />
       </div>
-      <div class="col-2">
+      <div class="col-12 col-md-2">
         <q-btn
           size="md"
           color="primary"
           label="Send"
           @click="getRequestUrl"
           icon-right="send"
+          class="full-width"
         />
+      </div>
+    </div>
+    <div class="row">
+      <div class="content-panel">
+        <div class="q-gutter-y-md data-panel">
+          <q-card>
+            <q-tabs
+              v-model="tab"
+              dense
+              class="text-grey"
+              active-color="primary"
+              indicator-color="primary"
+              align="justify"
+              narrow-indicator
+            >
+              <q-tab name="config" label="Config" />
+              <q-tab name="headers" label="Headers" />
+              <q-tab name="request" label="Request" />
+              <q-tab name="response" label="Response" />
+            </q-tabs>
+
+            <q-separator />
+
+            <q-tab-panels v-model="tab" animated>
+              <q-tab-panel name="config">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </q-tab-panel>
+
+              <q-tab-panel name="headers">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </q-tab-panel>
+
+              <q-tab-panel name="request">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </q-tab-panel>
+
+              <q-tab-panel name="response">
+                <div class="output-response">
+                  <pre id="pre-text"></pre>
+                  <!-- <pre
+                    >{{ text.length != 0 ? syntaxHighlight2(text) : "" }} </pre
+                  > -->
+                </div>
+              </q-tab-panel>
+            </q-tab-panels>
+          </q-card>
+        </div>
       </div>
     </div>
   </div>
@@ -41,7 +90,7 @@ export default {
   data() {
     return {
       request_type: { label: "GET", value: "GET" },
-      request_url: "",
+      request_url: "https://pegasusapi.genesisapps.in/holidays/getAllHolidays",
       request_options: [
         {
           label: "GET",
@@ -64,6 +113,8 @@ export default {
           value: "DELETE",
         },
       ],
+      tab: "request",
+      text: [],
     };
   },
   created() {
@@ -76,17 +127,80 @@ export default {
         alert("Please enter request URL.");
         return false;
       }
+      this.text = [];
       if (this.request_type.label == "GET") {
         axios
           .get(this.request_url)
           .then((response) => {
             console.log(response);
+            // let str = JSON.stringify(response.data, null, 2);
+            document.getElementById("pre-text").innerHTML =
+              this.syntaxHighlight(response.data);
           })
           .catch((error) => {
             console.log(error);
+            document.getElementById("pre-text").innerHTML = "";
           });
       }
       console.log("Got URL: ", this.request_type, " | ", this.request_url);
+    },
+    syntaxHighlight(json) {
+      if (typeof json != "string") {
+        json = JSON.stringify(json, undefined, 2);
+      }
+      json = json
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;");
+      let jb = json.replaceAll(
+        /("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g,
+        function (match) {
+          console.log("hello");
+          var cls = "number";
+          if (/^"/.test(match)) {
+            if (/:$/.test(match)) {
+              cls = "key";
+            } else {
+              cls = "string";
+            }
+          } else if (/true|false/.test(match)) {
+            cls = "boolean";
+          } else if (/null/.test(match)) {
+            cls = "null";
+          }
+          return '<span class="' + cls + '">' + match + "</span>";
+        }
+      );
+      console.log(jb);
+      // this.text = jb;
+      return jb;
+    },
+    syntaxHighlight2(json) {
+      if (typeof json != "string") {
+        json = JSON.stringify(json, undefined, 2);
+      }
+      console.log(json);
+      let jb = json.replaceAll(
+        /("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g,
+        function (match) {
+          console.log("hello");
+          var cls = "number";
+          if (/^"/.test(match)) {
+            if (/:$/.test(match)) {
+              cls = "key";
+            } else {
+              cls = "string";
+            }
+          } else if (/true|false/.test(match)) {
+            cls = "boolean";
+          } else if (/null/.test(match)) {
+            cls = "null";
+          }
+          return '<span class="' + cls + '">' + match + "</span>";
+        }
+      );
+      console.log(jb);
+      return jb;
     },
   },
 };
