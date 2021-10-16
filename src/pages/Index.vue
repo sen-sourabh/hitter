ae1a7c6b5b4058f89a176dc8e79b54b1c8f00c8b
Sourabh Sen <sourabhsen201313@gmail.com>
2021-10-16 13:48:39 +0530

    Request esing chages (16-10-2021)

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -93,49 +93,71 @@
         <div class="col-12 col-md-2">
           <q-btn
             size="md"
-            color="primary"
+            outline
             label="Send"
             @click="getRequestUrl"
             icon-right="send"
-            class="full-width"
+            class="full-width full-height"
           />
         </div>
       </div>
     <div>
       <q-splitter
         v-model="splitterModel"
+        separator-class="bg-orange"
         style="height: 500px;border: 1px solid lightblue;"
       >
 
         
         <template v-slot:before>
-          <q-splitter
-            v-model="insideModel"
-            horizontal
-          >
+          <div class="q-pa-md">
+            <div class="text-h6 q-mb-md">Request</div>
+            <div class="full-width">
+              <q-list bordered class="rounded-borders">
+                <q-expansion-item
+                  expand-separator
+                  icon="perm_identity"
+                  label="Authorizations"
+                >
+                  <q-card>
+                    <q-card-section>
+                      Authorizations
+                    </q-card-section>
+                  </q-card>
+                </q-expansion-item>
 
-            <template v-slot:before>
-              <div class="q-pa-md">
-                <div class="text-h4 q-mb-md">Before</div>
-                <div v-for="n in 20" :key="n" class="q-my-md">{{ n }}. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam. In, libero.</div>
-              </div>
-            </template>
+                <q-expansion-item
+                  expand-separator
+                  icon="signal_wifi_off"
+                  label="Headers"
+                >
+                  <q-card>
+                    <q-card-section>
+                      Headers
+                    </q-card-section>
+                  </q-card>
+                </q-expansion-item>
 
-            <template v-slot:after>
-              <div class="q-pa-md">
-                <div class="text-h4 q-mb-md">After</div>
-                <div v-for="n in 20" :key="n" class="q-my-md">{{ n }}. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam. In, libero.</div>
-              </div>
-            </template>
+                <q-expansion-item
+                  expand-separator
+                  icon="drafts"
+                  label="Request Body"
+                >
+                  <q-card>
+                    <q-card-section>
+                      Request Body
+                    </q-card-section>
+                  </q-card>
+                </q-expansion-item>
 
-          </q-splitter>
+              </q-list>
+            </div>
+          </div>
         </template>
-        
-        
-        
+
         <template v-slot:after>
           <div class="q-pa-md">
-            <div class="text-h4 q-mb-md">Response</div>
+            <div class="text-h6 q-mb-md">Response</div>
             <!-- <div > -->
               <pre class="q-my-md formatter" style="display:none;" id="pre-text"></pre>
             <!-- </div> -->
